3a4137e38249ef9044dfda2f9bcf2fc5a340a32f
尹凯 <805937131@qq.com>
2020-12-23 16:06:00 +0800

    eureka完成

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.controller;
 
 
+
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,18 +14,19 @@ import org.springframework.web.client.RestTemplate;
 public class OrderController {
 
     private final static String url = "http://127.0.0.1:8001";
+    private final static String name = "http://cloud-payment-service";
 
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment>   create(Payment payment){
-        return restTemplate.postForObject(url+"/payment/create",payment,CommonResult.class);  //写操作
+        return restTemplate.postForObject(name+"/payment/create",payment, CommonResult.class);  //写操作
     }
 
     @GetMapping("consumer/payment/get/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id){
-        CommonResult forObject = restTemplate.getForObject(url + "/payment/get/" + id, CommonResult.class);
+        CommonResult forObject = restTemplate.getForObject(name + "/payment/get/" + id, CommonResult.class);
         if (forObject!=null){  //说明有数据，能查询成功
             return new CommonResult(200,"查询成功",forObject);
         }else {
