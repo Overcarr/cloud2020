8ec9f6819400f0118868a0aca366e42e0fcfd379
尹凯 <805937131@qq.com>
2020-12-31 17:59:12 +0800

    nacos

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderController {
+
+    @Value("${service-url.nacos-user-service}")
+    private String defaultUrl;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @GetMapping("/consumer/payment/{id}")
+    public String payment(@PathVariable("id")String id){
+        return restTemplate.getForObject(defaultUrl+"/payment/" + id,String.class);
+
+    }
+}
